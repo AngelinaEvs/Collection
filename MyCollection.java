cee20e01cfef87a0f7c372b46a845ef7313c3c66
AngelinaEvs <57727402+AngelinaEvs@users.noreply.github.com>
2020-03-19 21:38:22 +0300

    Add files via upload

---
+++ MyCollection.java
@@ -0,0 +1,137 @@
+import java.util.*;
+
+public class MyCollection <T> {
+    private T[] data;
+    private int size;
+    private int maxsize = 10;
+
+    public MyCollection() {
+        this.data = (T[]) new Object[this.maxsize];
+        this.size = 0;
+    }
+
+    public MyCollection(Collection<? extends T> col) {
+        this.size = col.size();
+        this.data = (T[]) new Object[this.size];
+        int i = 0;
+        Iterator<T> it = (Iterator<T>) col.iterator();
+        while (it.hasNext()) {
+            this.data[i] = it.next();
+            i++;
+        }
+    }
+
+    private int size() {
+        return this.size;
+    }
+
+    public Iterator<T> iterator() {
+        return new StrangeArrayIterator();
+    }
+
+    private class StrangeArrayIterator <T> implements Iterator<T> {
+
+    protected int cursor;
+
+    public boolean hasNext() {
+        if (!(data.length > cursor)) {
+            return false;
+        }
+        for (int i = cursor; i < data.length; i = i + 2) {
+            if (data[i] != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T next() {
+        try {
+            T el = (T) data[cursor];
+            cursor = cursor + 2;
+            while (el == null) {
+                el = (T) data[cursor];
+                cursor = cursor + 2;
+            }
+            return el;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new NoSuchElementException();
+        }
+    }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyCollection<T> that = (MyCollection<T>) o;
+        if (size != that.size) return false;
+        boolean[] value = new boolean[that.data.length];
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < that.data.length; j++) {
+                if ((data[i].equals(that.data[j])) && !(value[j])) {
+                    value[j] = true;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < value.length; i++) {
+            if (!(value[i]) && i == value.length - 1 ) {
+                return false;
+            }
+        }
+        return true;
+        /*for (int i = 0; i < value.length; i++) {
+            if (value[i] && i == value.length - 1 ) {
+                return true;
+            }
+            if (!(value[i])) {
+                return false;
+            }
+        }
+        return false;*/
+    }
+
+    /*@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyCollection<T> that = (MyCollection<T>) o;
+       if (size != that.size) return false;
+        boolean f = true;
+        for (int i = 0; i < data.length; i++) {
+            if (!f) break;
+            for (int j = i; j < that.data.length; j++) {
+                if (data[i].equals(that.data[j])) {
+                    T el = that.data[j];
+                    that.data[j] = that.data[i];
+                    that.data[i] = el;
+                    break;
+                }
+                if (j == that.data.length - 1 && !(data[i].equals(that.data[j]))) {
+                    f = false;
+                    break;
+                }
+            }
+        }
+        return f;
+    }*/
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(size, maxsize);
+        result = 31 * result + Arrays.hashCode(data);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "MyCollection{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                ", maxsize=" + maxsize +
+                '}';
+    }
+}
+
+
